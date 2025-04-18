8ec867a1abbca8e615aa955d1563035d56c00e16
IBRA-oub <brahimbourih@gmail.com>
2025-04-18 12:16:46 +0100

    LGM-4 configurer webpack

--- wp-content/themes/le_grand_marche/functions.php
+++ wp-content/themes/le_grand_marche/functions.php
@@ -182,3 +182,24 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+/**
+ * Enqueue styles.
+ */
+
+function theme_enqueue_styles() {
+    wp_enqueue_style('theme-styles', get_template_directory_uri() . '/assets/css/style.css', array(), '1.0.0');
+}
+add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+
+/**
+ * Constum fonts
+ */
+
+ function enqueue_custom_fonsts(){
+	if(!is_admin()){
+		wp_register_style('poppins','https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swa');
+	}
+ } 
+
+ add_action('wp_enqueue_script','enqueue_custom_fonsts');
